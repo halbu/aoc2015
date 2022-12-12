23305159fd6fb93b4ffe92775d49c25365e1b216
halbu <fstream.c@gmail.com>
2022-12-12 21:12:44 +0000

    Day 19 part 2

--- day19/solution.js
+++ day19/solution.js
@@ -10,7 +10,7 @@ d.forEach(line => {
   } else if (line !== "") molecule = line
 })
 
-function repl(mol, ix) {
+function findReplacementPossibilitiesAtIndex(mol, ix) {
   for (let i = 0; i <=2; ++i) {
     var segment = mol.slice(ix, ix + i)
     if (M[segment]) {
@@ -22,7 +22,33 @@ function repl(mol, ix) {
 }
 
 for (var i = 0; i != molecule.length; ++i) {
-  repl(molecule, i)
+  findReplacementPossibilitiesAtIndex(molecule, i)
 }
 
 console.log("Day 19 part 1 solution: " + [...new Set(possibilities)].length)
+
+function recursiveReduce(mol, steps) {
+  if (mol === "e") {
+    console.log("Day 19 part 2 solution: " + steps)
+    return
+  }
+
+  var replacements = []
+
+  for(var i = 0; i != mol.length - 1; ++i) {
+    for(var j = 0; i + j <= mol.length && j <= 10; ++j) {
+      var seg = mol.slice(i, i+j)
+      Object.entries(M).forEach(e => {
+        if (e[1].includes(seg)) {
+          replacements.push({ in: seg, out: e[0], len: seg.length })
+        }
+      })
+    }
+  }
+
+  replacements = replacements.sort((a, b) => a.len - b.len);  // We just reduce by the biggest replaceable chunk
+  var repl = replacements.pop()                               // each time. I thought this was going to require
+  recursiveReduce(mol.replace(repl.in, repl.out), steps + 1)  // branching recursion, but the naive method works.
+}
+
+recursiveReduce(molecule, 0)
