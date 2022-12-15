f323b9b61bc65b4f3773799ac035777c90ed64b8
halbu <fstream.c@gmail.com>
2022-12-15 02:01:20 +0000

    Day 21 slightly more elegant, slightly less code

--- day21/solution.js
+++ day21/solution.js
@@ -1,53 +1,37 @@
 let d = require('fs').readFileSync('data.txt').toString().split("\n").map(x => x.trim());
 W = [], A = [], R = [], loadouts = []
 
-for (var i = 1; i <= d.length; ++i) {
+for (let i = 1; i != 21; ++i) {
+  let slices = d[i].split(/[ ]+/);
   if (i >= 1 && i <= 5) {
-    let wpn = d[i].split(/[ ]+/);
-    W.push([parseInt(wpn[1]), parseInt(wpn[2]), parseInt(wpn[3]), wpn[0]])
+    W.push([ parseInt(slices[1]), parseInt(slices[2]), parseInt(slices[3]), slices[0]])
   } else if (i >= 8 && i <= 12) {
-    let arm = d[i].split(/[ ]+/);
-    A.push([parseInt(arm[1]), parseInt(arm[2]), parseInt(arm[3]), arm[0]])
+    A.push([ parseInt(slices[1]), parseInt(slices[2]), parseInt(slices[3]), slices[0]])
   } else if (i >= 15 && i <= 20) {
-    let rng = d[i].split(/[ ]+/);
-    R.push([parseInt(rng[2]), parseInt(rng[3]), parseInt(rng[4]), rng[0] + rng[1]])
+    R.push([ parseInt(slices[2]), parseInt(slices[3]), parseInt(slices[4]), slices.slice(0, 1)])
   }
 }
+A.push([0, 0, 0, "none"])
+R.push([0, 0, 0, "none"])
 
 // Iterate all gear possibilities and store in `loadouts`, starting with weapons.
-[...Array(A.length)].forEach((_, i) => chooseArmor(W[i][0], W[i][1], W[i][2]))
-
-function chooseArmor(cost, damage, armor) {
-  [...Array(A.length)].forEach((_, i) => chooseRing1(cost + A[i][0], damage + A[i][1], armor + A[i][2]))
-  chooseRing1(cost, damage, armor) // Don't wear armor
-}
-
-function chooseRing1(cost, damage, armor) {
-  [...Array(R.length)].forEach((_, i) => chooseRing2(cost + R[i][0], damage + R[i][1], armor + R[i][2], R[i][3]))
-  chooseRing2(cost, damage, armor, "none") // Don't wear a ring in ring slot 1
-}
-
-function chooseRing2(cost, damage, armor, nameOfRing1) {
-  [...Array(R.length)].forEach((_, i) => {
-    if (R[i][3] !== nameOfRing1) { // Can't wear duplicate rings
-      storeGearCombination(cost + R[i][0], damage + R[i][1], armor + R[i][2])
-    }
+W.forEach(w => {
+  A.forEach(a => {
+    R.forEach(r => {
+      R.forEach(r2 => {
+        if (r[3] === r2[3]) return; // No duplicate rings
+        loadouts.push({ cost: w[0]+a[0]+r[0]+r2[0], dmg: w[1]+a[1]+r[1]+r2[1], def: w[2]+a[2]+r[2]+r2[2]})
+      })
+    })
   })
-  storeGearCombination(cost, damage, armor) // Don't wear a ring in ring slot 2
-}
-
-function storeGearCombination(cost, damage, armor) { loadouts.push({ cost, damage, armor }) }
-
-let lowestWinningCost = 999, highestLosingCost = 0;
-loadouts.forEach(c => {
-  if (fight(c.armor, c.damage) && c.cost < lowestWinningCost) lowestWinningCost = c.cost
-  if (!fight(c.armor, c.damage) && c.cost > highestLosingCost) highestLosingCost = c.cost
 })
-console.log("Day 21 part 1 solution: " +  lowestWinningCost);
-console.log("Day 21 part 2 solution: " +  highestLosingCost);
 
-function fight(playerArmor, playerDamage) {
-  let player = { hp: 100, armor: playerArmor, damage: playerDamage }
+loadouts.forEach(l => { l.won = fight(l.dmg, l.def) })
+console.log("Day 21 part 1 solution: " + loadouts.filter(x => x.won).sort((a, b) => a.cost > b.cost ? 1 : -1)[0].cost)
+console.log("Day 21 part 2 solution: " + loadouts.filter(x => !x.won).sort((a, b) => a.cost < b.cost ? 1 : -1)[0].cost)
+
+function fight(dmg, def) {
+  let player = { hp: 100, armor: def, damage: dmg }
   let boss = { hp: 104, armor: 1, damage: 8 }
 
   while (player.hp > 0 && boss.hp > 0) {
